94ac393eacdd660c276909dd2103194247ccb61c
TheBinKingVII <bintoronob@gmail.com>
2025-12-16 19:52:13 +0700

    uodate migration lagi

--- apps/server/prisma/migrations/20250521151357_make_keterangan_in_anggota_ekstrakurikuler_optional/migration.sql
+++ apps/server/prisma/migrations/20250521151357_make_keterangan_in_anggota_ekstrakurikuler_optional/migration.sql
@@ -1,2 +1,2 @@
--- AlterTable
-ALTER TABLE `anggota_ekstrakurikuler` MODIFY `keterangan` VARCHAR(191) NULL;
+-- NOTE: keep table name casing consistent with creation in 20250521151035_add_anggota_kelas
+ALTER TABLE `Anggota_Ekstrakurikuler` MODIFY `keterangan` VARCHAR(191) NULL;
